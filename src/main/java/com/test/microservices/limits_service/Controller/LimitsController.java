bd84d4df9fa9ee449f296540082a456583312791
Kamal Chaudhari <kamalchaudhary502@gmail.com>
2024-07-15 23:43:53 +0530

    Using Application properties to configure limits

--- src/main/java/com/test/microservices/limits_service/Controller/LimitsController.java
+++ src/main/java/com/test/microservices/limits_service/Controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.test.microservices.limits_service.Controller;
 
 import com.test.microservices.limits_service.Beans.Limits;
+import com.test.microservices.limits_service.Configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retriveLimits(){
-        return new Limits(1,500);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
